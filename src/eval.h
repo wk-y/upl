0f051691135098b99abe55ed769d5f1edade7e5d
wk-y <86206045+wk-y@users.noreply.github.com>
2025-03-04 12:09:13 -0800

    stub interpreter stuff

--- src/eval.h
+++ src/eval.h
@@ -4,6 +4,24 @@
 #include "ast.h"
 #include "value.h"
 
+struct variables {
+  char *name;
+  struct value value;
+  struct variables *next;
+};
+
+struct interpreter {
+  struct variables *variables;
+};
+
+void interpreter_init(struct interpreter *);
+void interpreter_deinit(struct interpreter *);
+
+struct value *interpreter_get_variable(struct interpreter, char *name);
+
+struct value interpreter_eval(struct interpreter *, struct ast_node *);
+
+// Creates an interpreter to use
 struct value eval(struct ast_node *);
 
 #endif
