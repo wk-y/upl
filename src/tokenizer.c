542a3d1f7c4414307aa818b504613e93be1115ca
wk-y <86206045+wk-y@users.noreply.github.com>
2025-03-03 17:02:37 -0800

    Better tokenizer

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -1,4 +1,5 @@
 #include "tokenizer.h"
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -9,6 +10,12 @@ void tokenizer_init(struct tokenizer *t) {
   if (!(t->literal = malloc(t->literal_cap))) {
     abort();
   };
+}
+
+bool literal_char_p(char c) {
+  return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') ||
+         (c >= '0' && c <= '9');
+  ;
 }
 
 void tokenizer_deinit(struct tokenizer *t) { free(t->literal); }
@@ -48,9 +55,9 @@ void tokenizer_feed(struct tokenizer *t, FILE *f) {
     t->token_type = tt_rpar;
     t->literal_len = 0;
     t->literal[0] = 0;
-  } else {
+  } else if (literal_char_p(c)) {
     t->token_type = tt_literal;
-    while (c != ' ' && c != EOF && c != '\n' && c != '(' && c != ')') {
+    while (literal_char_p(c)) {
       t->literal[t->literal_len++] = c;
       c = getc(f);
     }
